443b8e1aec66de31dee6afae9efe6bf91ea0754e
Ghislain1 <gzeleu@outlook.de>
2023-07-23 00:04:34 +0200

    Init project ....

---
+++ src/YAWS/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿// <copyright company="Ghislain One Inc.">
+//  Copyright (c) GhislainOne
+//  This computer program includes confidential, proprietary
+//  information and is a trade secret of GhislainOne. All use,
+//  disclosure, or reproduction is prohibited unless authorized in
+//  writing by an officer of Ghis. All Rights Reserved.
+// </copyright>
+
+namespace YAWS;
+
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+
+    public App()
+    {
+
+    }
+
+
+}
